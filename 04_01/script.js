3ae01f66f079961bf3fbb694c809a2b99610c489
Eric Snyder <guideon72@hotmail.com>
2022-09-09 10:32:22 -0700

    Added template literal example to project for injecting text/code directly into an HTML element

--- 04_01/script.js
+++ 04_01/script.js
@@ -6,7 +6,7 @@
 import Backpack from "./Backpack.js";
 
 const everydayPack = new Backpack(
-  "Everyday Backpack",
+  "Bob",
   30,
   "grey",
   15,
@@ -15,6 +15,34 @@ const everydayPack = new Backpack(
   false,
   "December 5, 2018 15:00:00 PST"
 );
+
+//Create a template literal by setting a constant and filling in the template data between two backticks.  This allows the script to inject a combination of text (html) and code (JS). Template code is denoted by ${}
+const content = `
+  <main>
+    <article>
+      <h1>${everydayPack.name}</h1>
+      <ul>
+        <li>Volume: ${everydayPack.volume}</li>
+        <li>Color: ${everydayPack.color}</li>
+        <li>Age:</li>
+        <li>Number of pockets:</li>
+        <li>Left strap length: ${everydayPack.strapLength.left}</li>
+        <li>Right strap length:</li>
+        <li>Lid status:</li>
+      </ul>
+    </article>
+  </main>;
+`;
+
+document.body.innerHTML = content;
+
+
+
+
+
+
+
+
 
 console.log("The everydayPack object:", everydayPack);
 console.log("The pocketNum value:", everydayPack.pocketNum);
